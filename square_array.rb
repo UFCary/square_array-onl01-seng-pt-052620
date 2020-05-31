f715308a4c6ccf575941a078ad43e8829d6d64bf
Cary Williams <cary.l.williams00@gmail.com>
2020-05-31 18:51:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,9 +2,7 @@ def square_array(array)
   #create empty array 
   new_numbers = []
   array.each do |numbers|
-    puts "Current number is #{numbers}"
-    #shovel eah squared number into empty array
-    1 * l
+    
 
     #return the variable of the empty array created above
    
