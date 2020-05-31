3ba9c6eed879dd2bbb87b111e26f68cf4f8972aa
Cary Williams <cary.l.williams00@gmail.com>
2020-05-31 17:55:49 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,8 @@
 def square_array(array)
-  
+  #create empty array 
   array.each do |numbers|
-    puts "This is test #{numbers}"
+    #puts "This is test #{numbers}"
+    #shuffle eah squared number into empty array
+    #return the variable of the empty array created above
   end
 end
